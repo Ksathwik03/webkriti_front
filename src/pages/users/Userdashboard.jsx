5d3d150fef38dbad6736ae282ca6008ea6509177
Ksathwik03 <iit2020241@iiita.ac.in>
2022-04-08 14:43:39 +0530

    Fixed bugs of allocation

--- src/pages/users/Userdashboard.jsx
+++ src/pages/users/Userdashboard.jsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
-// import { user_data } from "../../data";
 import { useHistory } from "react-router-dom";
-// import * as rooms from '../../Api/api';
 import { ImCross } from "react-icons/im";
 import { BiRightArrow } from "react-icons/bi";
 import "../assets/styles/userdashboard.css";
@@ -11,114 +9,63 @@ import { toast, ToastContainer } from "react-toastify";
 
 function Userdashboard({ match }) {
   const [user_data, setuser_data] = useState({
-    username: localStorage.getItem("username") || "",
+    username: localStorage.getItem("email") || "",
     user_id: localStorage.getItem("user_id") || "",
     All_Requests: [],
   });
   const [search, setsearch] = useState("");
   const [roomsDetail, setroomsDetail] = useState([]);
   const [showRequestForm, setshowRequestForm] = useState(false);
+  const [showAllocationForm, setshowAllocationForm] = useState(false);
   const [blur, setblur] = useState(false);
   const [loading, setloading] = useState(false);
-  const [productLink, setproductLink] = useState("");
-  const [bank, setbank] = useState("ICIC");
-  const [update, setupdate] = useState(false);
   const history = useHistory();
   const [prodDet, setprodDet] = useState({});
   const [showpro, setshowpro] = useState(false);
-  const [offer, setoffer] = useState({ name: "", price: "", image: "" });
-
+  const [roomNo, setroomNo] = useState('');
+  const [StudentName, setStudentName] = useState('');
+  const [EnrollmentNo, setEnrollmentNo] = useState('');
+  const [MobileNo, setMobileNo] = useState('');
+  
   let admin = false;
-  if (localStorage.getItem("username") == "admin") {
+  if (localStorage.getItem("admin") !== null) {
     admin = true;
   }
-  // const getdetails = () => {
-  //   rooms.rooms.map((item) => {
-  //     if (item.link === productLink){
-  //       setoffer({name:item.name,price:item.price,image:item.image})
-  //     }
-  //   })
-  //   setshowRequestForm(false)
-  //   // setloading(true)
-  //   // await fetch(productLink).then(res => setprodDet(res.data))
-  //   // axios.get(productLink).then((res) => {setprodDet(res.data);setloading(false);setshowpro(true)})
-  //   setshowpro(true)
-  // }
 
   const filterFucntion = ({ product_name }) => {
     return product_name.toLowerCase().indexOf(search.toLowerCase().trim()) > -1;
   };
 
   const [rooms, setrooms] = useState([]);
-  // useEffect(() => {
-  //   if (search === "") {
-  //     setfilteredRequests(user_data.All_Requests);
-  //   }
-  //   setfilteredRequests(user_data.All_Requests.filter(filterFucntion));
-  // }, [search]);
 
   useEffect(async () => {
     setloading(true);
     const data = await axios.get(`${api}/room`);
-    // console.log(data.data.products)
     setroomsDetail(data.data.products);
     console.log(roomsDetail[0])
     setloading(false);
   },[rooms]);
-  // useEffect(async () => {
-  //   setloading(true)
-  //   await axios.post(`${api}product/user`, headers)
-  //     .then((res) => {
-  //       if (res.status == 200) {
-  //         console.log(res)
-  //         let temp_data = user_data
-  //         temp_data.All_Requests = res.data.rooms
-  //         setuser_data(temp_data)
-  //         setfilteredRequests(res.data.rooms)
-  //       }
-  //       else {
-  //         toast("some error occured")
-  //       }
-  //       setloading(false)
-  //     })
-  // }, [update])
 
-  // function tokenHandler(token){
-  //   console.log(token)
-  //   postProduct()
-  // }
+  const Allocate = async(req,res) => {
+    setloading(true)
+    const temp = {
+      student_name: StudentName,
+      MobileNumber: MobileNo,
+      EnrollmentNumber: EnrollmentNo
+    }
+    
+    const data = await axios.post(`${api}/room/allocate/${roomNo}`,temp)
+    
+    setloading(false)
+  }
 
-  // const postProduct = async (e) => {
-  //   // e.preventDefault()
-  //   setloading(true)
-  //   const body = { product_name: productName, product_link: productLink, bank: bank ,"token": localStorage.getItem("token") || null }
-  //   await axios.post(`${api}product/add`, body)
-  //     .then((res) => {
-  //       console.log(res)
-  //       if (res.status == 200) {
-  //         toast("Successfully added")
-  //       }
-  //       else {
-  //         toast("Some error occured")
-  //       }
-  //     })
-  //   setblur(false)
-  //   setprodDet({});
-  //   setshowpro(false)
-  //   setupdate(!update)
-  //   setloading(false)
-  // }
+  const vacate = async(req,res) => {
+    console.log(roomNo)
+    setloading(true)
 
-  // const handleProduct = async (id) => {
-  //   console.log(id)
-  //   const headers = { "x-access-token": localStorage.getItem("token") || null }
-  //   const body = { dealStatus: "success" }
-  //   await axios.put(`${api}product/update/${id}`, headers, body)
-  //     .then((res) => {
-  //       console.log(res)
-  //     })
-  //   setupdate(!update)
-  // }
+    const data = await axios.post(`${api}/room/vacate/${roomNo}`)
+    setloading(false)
+  }
 
   if (loading) {
     return (
@@ -128,10 +75,12 @@ function Userdashboard({ match }) {
     );
   }
 
+
+
   return (
     <>
       <div className={`dashboard-main ${blur ? "mkblr" : ""}`}>
-        <h1 className="user-name"> Welcome {user_data.username}</h1>
+        <h1 className="user-name"> Welcome {localStorage.getItem("email")}</h1>
         <div className="dashboard-box">
           {" "}
           <div className="filter">
@@ -141,7 +90,7 @@ function Userdashboard({ match }) {
               onChange={(e) => setsearch(e.target.value)}
               value={search}
             />
-            {!admin && (
+            {admin && (
               <button
                 onClick={() => {
                   setshowRequestForm(!showRequestForm);
@@ -168,31 +117,45 @@ function Userdashboard({ match }) {
                     {!el.isEmpty&&<span>Mobile No.:{el.MobileNumber}</span>}<br />
                     {!el.isEmpty&&<span></span>}
                   </div>
-                  {/* {admin && (
+                  {admin && el.isEmpty &&(
                     <button
                       className="chat"
-                      onClick={() =>
-                        history.push(
-                          `/${match.params.userId}/dashboard/${request.chat_id}`
-                        )
-                      }
+                      onClick={() => {
+                        setshowAllocationForm(!showAllocationForm);
+                        setblur(!blur);
+                      }}
                     >
                       Allocate{"   "} <BiRightArrow />
                     </button>
-                  )} */}
+                  )}
+                  
+                  {admin && !el.isEmpty &&(
+                    <button
+                      className="chat"
+                      onClick={() => {
+                        setroomNo(el.roomNumber)
+                        vacate()
+                      }}
+                    >
+                      Vacate{"   "} <BiRightArrow />
+                    </button>
+                  )}
+
                 </div>
               ))}
             </div>
           </div>
         </div>
       </div>
-      {showRequestForm && (
+      
+
+      {showAllocationForm && (
         <div className="requestForm">
           <div className="form-head">
-            <h1>Filter Rooms</h1>
+            <h1>Allocate Rooms</h1>
             <button
               onClick={() => {
-                setshowRequestForm(!showRequestForm);
+                setshowAllocationForm(false);
                 setblur(!blur);
                 setprodDet({});
                 setshowpro(false);
@@ -207,33 +170,67 @@ function Userdashboard({ match }) {
             <input
               type="text"
               name="link"
-              placeholder="product link"
+              placeholder="Room No."
               onChange={(e) => {
-                setproductLink(e.target.value);
+                setroomNo(parseInt(e.target.value));
               }}
               required
             />
           </div>
 
           <div className="field">
-            <label htmlFor="bank">Avaliable Stauts</label>
-            <select name="status" onChange={(e) => setbank(e.target.value)}>
-              <option value="True">True</option>
-              <option value="False">False</option>
-            </select>
+            <label htmlFor="link">StudentName</label>
+            <input
+              type="text"
+              name="link"
+              placeholder="Student Name"
+              onChange={(e) => {
+                setStudentName(e.target.value);
+              }}
+              required
+            />
           </div>
+          
+          <div className="field">
+            <label htmlFor="link">Enrollment number</label>
+            <input
+              type="text"
+              name="link"
+              placeholder="Enrollment Number"
+              onChange={(e) => {
+                setEnrollmentNo(e.target.value);
+              }}
+              required
+            />
+          </div>
+
+          <div className="field">
+            <label htmlFor="link">MobileNo</label>
+            <input
+              type="text"
+              name="link"
+              placeholder="Mobile Number"
+              onChange={(e) => {
+                setMobileNo(e.target.value);
+              }}
+              required
+            />
+          </div>
+
           <button
             onClick={() => {
-              setshowRequestForm(!showRequestForm);
+              setshowAllocationForm(false);
               setblur(!blur);
               setprodDet({});
               setshowpro(false);
+              Allocate();
             }}
           >
-            Filter
+            Allocate
           </button>
         </div>
       )}
+
       <ToastContainer />
     </>
   );
